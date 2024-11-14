a6b85b03742bbd8edbfde2ea2c949c83e97d6b7b
Peter Kurfer <peter.kurfer@rwe.com>
2024-11-14 14:21:25 +0100

    feat(ci): initial pipeline

--- generate.go
+++ generate.go
@@ -1,3 +1,3 @@
 package main
 
-//go:generate go run -mod=mod github.com/apple/pkl-go/cmd/pkl-gen-go assets/AppConfig.pkl --base-path github.com/prskr/go-dito
+//go:generate go run -modfile=tools/go.mod github.com/apple/pkl-go/cmd/pkl-gen-go assets/AppConfig.pkl --base-path github.com/prskr/go-dito
